73bf8cbef873d1861f569ba8e58b6c808da1f713
710599514@qq.com <710599514@qq.com>
2016-02-22 11:52:56 +0800

    add container_of

--- offset.cpp
+++ offset.cpp
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <string.h>
 #include <iostream>
 
 typedef struct sOffset
@@ -10,10 +11,17 @@ typedef struct sOffset
 
 //only use c struct, can't use class
 #define OFFSETOF(st, m) ((size_t)(&(((st* )0)->m)))
+#define container_of(ptr, type, member) ({\
+       typeof(((type* )0)->member)* __mptr=(ptr);\
+        (type* )((char* )__mptr-OFFSETOF(type, member));\
+        })
 
 int main(void)
 {
     SOFFSET st;
+    st.a = 10;
+    strncpy(st.b, "hello world", sizeof(st.b));
+    st.ptr = NULL;
     size_t off = OFFSETOF(SOFFSET, a);
     std::cout <<"a offset: " <<  off << std::endl;
     off = OFFSETOF(SOFFSET, b) ;
@@ -21,4 +29,7 @@ int main(void)
     off =OFFSETOF(SOFFSET, ptr) ;
     std::cout <<"ptr offset: " << off << std::endl;
 
+    SOFFSET* ptr;
+    ptr = container_of(&(st.a), SOFFSET, a);
+    std::cout <<"container_of ptr->b" << ptr->b << std::endl;
 }
