b471f7b708d6dc8c7f063f6e8dbad170d49e5896
710599514@qq.com <710599514@qq.com>
2016-03-21 18:17:06 +0800

    commit
    array swap is different with other container like vector

--- array.cpp
+++ array.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <array>
+#include "comm_tool.h"
 
 using namespace std;
 
-int main(void)
+int main(int argc, char** argv)
 {
-    array<int, 5> a = {1, 2, 3, 4, 5};
+    const int size =  10000;
+    array<int, size> a;
+    a.fill(1);
+
+    array<int, size> b;
+    b.fill(100);
+    CTimeCost timec;
+    a.swap(b);
+    cout << "time :" << timec.value() << endl;
+
+    vector<int> veca(size, 1);
+    vector<int> vecb(size, 100);
+
+    CTimeCost timevec;
+    veca.swap(vecb);
+    cout << "size is : " << size << "\ttime :" << timevec.value() << endl;
 
     array<int, 5>::iterator iter;
     int i = 0;
-    for (auto it=a.begin(); it!=a.end(); it++)
+    for (auto it=a.begin(); it!=a.end(), i<5; it++)
     {
-        cout <<"element " << i++ << "\t:" <<  *it;
+        cout <<"element " << i++ << " :" <<  *it << "\t";
     }
 
     cout << endl;
